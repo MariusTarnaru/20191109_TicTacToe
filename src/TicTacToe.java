70cf4e09d4dc34c86a86e6fb0ebdff3ccb8bb375
MariusTarnaru <mtarnaru@yahoo.com>
2019-11-09 12:27:46 +0200

    s-a initializat schimbarea intre playeri

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,32 +15,57 @@ public class TicTacToe {
         this.player1 = player1;
         this.player2 = player2;
     }
+
     // puts spaces in all locations on board
-    public void initBoard(){
-        for (int i = 0; i < board.length ; i++) {
+    public void initBoard() {
+        for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 board[i][j] = ' ';
             }
         }
     }
-    public void showBoard(){
+
+    public void showBoard() {
         System.out.println("Board: ");
         int x;
-        for (int i = 0; i < SIZE ; i++) {
+        for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-
-                x = (i * SIZE + j)+1;
- //               System.out.print(board[i][j] + " ");
+                // TODO: must determine how i want to show the values on the board
+                x = (i * SIZE + j) + 1;
+                //               System.out.print(board[i][j] + " ");
                 System.out.print(x + " ");
             }
             System.out.println();
         }
     }
 
-    public void playGame(){
+    public int readMove() {
+        //temp
+        return -1;
+    }
+
+    public void makeMove(Player player, int position) {
+
+    }
+
+    public void playGame() {
         // initBoard
         initBoard();
         showBoard();
 
+        Player currentPlayer = player1;
+        //while not win
+            //read move
+            readMove();
+            //make move
+             //show board
+            //change player ?
+             if (currentPlayer == player1) {
+             currentPlayer = player2;
+             }else {
+                 currentPlayer = player1;
+             }
+
+        }
     }
 }
